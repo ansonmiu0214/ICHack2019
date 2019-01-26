09203d64e9ea6f29966f1eaa4b209b96b6456d02
Anson Miu <ansonmiu0214@gmail.com>
2019-01-26 19:51:42 +0000

    Added mapView

--- TravelForAll/MapViewController.swift
+++ TravelForAll/MapViewController.swift
@@ -9,15 +9,71 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
   @IBOutlet weak var mapView: MKMapView!
+  
+  let speaker = Speaker()
+  
+  var coordFrom: CLLocationCoordinate2D? = nil
+  var navigateTo: Merchant? = nil
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
+    print("Map did load!")
     
+    self.mapView.delegate = self
+    computeDirections()
+  }
+  
+  func computeDirections() {
+    // Error handling for undefined values
+//    guard var from = coordFrom else {
+//      print("FROM coordinate is nil")
+//      return
+//    }
+    
+    guard var merchant = navigateTo else {
+      print("MERCHANT is nil")
+      return
+    }
+    
+    speaker.textToSpeech("Computing directions to \(merchant.name).")
+    
+    // Setup request
+    let request  = MKDirections.Request()
+    
+    let testFrom = CLLocationCoordinate2D(latitude: 51.4944, longitude: -0.1827)
+    let testTo = CLLocationCoordinate2D(latitude: 51.4988, longitude: -0.1749)
+    
+    let from = testFrom
+    let to = testTo
+    
+    request.source = MKMapItem(placemark: MKPlacemark(coordinate: from))
+    request.destination = MKMapItem(placemark: MKPlacemark(coordinate: to))
+    
+    request.transportType = .walking
+    
+    let directions = MKDirections(request: request)
+    
+    directions.calculate { [unowned self] response, error in
+      guard let unwrappedResponse = response else { return }
+      
+      
+      for route in unwrappedResponse.routes {
+        self.mapView.addOverlay(route.polyline)
+        self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+      }
+      
+    }
+  }
+  
+  func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+    let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+    renderer.strokeColor = UIColor.blue
+    return renderer
   }
   
   override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
--- TravelForAll/MerchantViewController.swift
+++ TravelForAll/MerchantViewController.swift
@@ -11,19 +11,38 @@ import UIKit
 
 class MerchantViewController: UIViewController {
   
+  let speaker = Speaker()
   var nearbyMerchants: [Merchant] = []
+  var handleTapResonse = true
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // Update merchants
     nearbyMerchants = getMerchants()
+    
+    // Double tap gesture recogniser
+    let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
+    tap.numberOfTapsRequired = 2
+    view.addGestureRecognizer(tap)
+  }
+  
+  @objc func doubleTapped() {
+    if !handleTapResonse { return }
+    
+    speaker.textToSpeech("You double tapped!") { [unowned self] in
+      
+      // Pass data to map
+      let mapVC = self.tabBarController?.viewControllers![1] as! MapViewController
+      
+      mapVC.navigateTo = self.nearbyMerchants[0]
+      
+      self.tabBarController?.selectedIndex = 1
+    }
   }
   
   override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-    
-    let speaker = Speaker()
-//    var speech = ""
-    
+  
     let merchantCount = nearbyMerchants.count
     if merchantCount == 0 {
       speaker.textToSpeech("There are no nearby merchants.")
@@ -31,15 +50,13 @@ class MerchantViewController: UIViewController {
       speaker.textToSpeech("There are \(merchantCount) nearby merchants.")
       for merchant in nearbyMerchants {
         speaker.textToSpeech(merchant.toSpokenString())
-//        speech += merchant.toSpokenString()
+      }
+      
+      speaker.textToSpeech("To get directions to your nearest merchant, double tap.") { [unowned self] in
+        self.handleTapResonse = true
       }
     }
     
-//    Speaker().textToSpeech(speech) {
-//      print("Finished speaking!")
-//    }
   }
   
 }
-
-
