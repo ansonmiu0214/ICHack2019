bbb2c24f959d6b628a6485f3bf76a9f35044a1fe
Anson Miu <ansonmiu0214@gmail.com>
2019-01-26 15:20:13 +0000

    Initial Commit

---
+++ TravelForAll/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TravelForAll
+//
+//  Created by Anson Miu on 26/1/2019.
+//  Copyright © 2019 Anson Miu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ TravelForAll/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TravelForAll
+//
+//  Created by Anson Miu on 26/1/2019.
+//  Copyright © 2019 Anson Miu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
